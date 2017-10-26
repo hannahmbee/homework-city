41e99bd5fd80e89e5d1409d193adf1e96eee2751
hannahmbee <31848924+hannahmbee@users.noreply.github.com>
2017-10-26 10:42:57 +0100

    pseudo code

---
+++ starter_code/js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+//store user input in var city
+
+//listen for 'submit' event
+
+//if user submits nyc
+	//.attr(set nyc background image)
+	//else do nothing
+
+//if user submits syd
+	//.attr(set syd background image)
+	//else do nothing
+
+//if user submits atx
+	//.attr(set atx background image)
+	//else do nothing
+
+//if user submits sf
+	//.attr(set sf background image)
+	//else do nothing
+
+//if user submits la
+	//.attr(set la background image)
+	//else do nothing
+
+
+});
+
+
+
+
+
+
